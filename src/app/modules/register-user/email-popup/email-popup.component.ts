1faf01f532e0be013ef42d43fbea4a9f91b26530
Alexandre <alexandre.costa22@outlook.com>
2024-10-16 18:24:15 -0300

    Finalização layout

--- src/app/modules/register-user/email-popup/email-popup.component.ts
+++ src/app/modules/register-user/email-popup/email-popup.component.ts
@@ -7,19 +7,29 @@ import { MatDialogRef } from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 @Component({
   selector: 'app-email-popup',
   standalone: true,
   templateUrl: './email-popup.component.html',
-  imports: [CommonModule, FormsModule, MainPageComponent, MatInputModule, MatFormFieldModule],
+  imports: [CommonModule, FormsModule, MainPageComponent, MatInputModule, MatFormFieldModule, MatProgressSpinnerModule],
   styleUrls: ['./email-popup.component.css']
 })
 
 export class EmailPopupComponent {
   constructor(public dialogRef: MatDialogRef<EmailPopupComponent>) {}
 
+  public gatilhoSpinner : boolean = false;
+  public gatilhoConfirma: boolean = false;
+
   // Método para fechar o diálogo
   closeDialog(): void {
     this.dialogRef.close();
   }
+
+  cadastra(){
+    this.gatilhoSpinner = true;
+  }
+
 }
